bdec5a32e62d00dda2b35c67b7d601a435cceab5
hemant-collab <hemantagrahari05@gmail.com>
2025-12-31 05:24:46 +0530

    Weight Convertor

---
+++ Ternary_Operator.java
@@ -0,0 +1,9 @@
+
+public class Ternary_Operator {
+    public static void main(String [] args){
+
+        // ternary operator ? = Return 1 of 2 values if a condition is true
+
+        // variable = (condition) ? ifTrue : ifFalse;
+    }
+}
