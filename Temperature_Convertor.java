02a0ed8a42ba04e1942cde052daa27453ee1a273
hemant-collab <hemantagrahari05@gmail.com>
2025-12-31 06:02:34 +0530

    Temperature Convertor

---
+++ Temperature_Convertor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Temperature_Convertor {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter the temperature value: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to celsius or fahrenheit (C/F): ");
+        unit = scanner.next().toUpperCase();
+         
+        // (condition) ? true : false
+        newTemp = (unit.equals("C")) ? (temp - 32)* 5/9 : (temp * 5/9) + 32;
+
+        System.out.printf("%.1f°%s", newTemp, unit);
+
+        scanner.close();
+    }
+    
+}
