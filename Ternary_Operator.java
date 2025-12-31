448f589af7881881e531dcb4d2b17b8df07b83cc
hemant-collab <hemantagrahari05@gmail.com>
2025-12-31 05:34:04 +0530

    Ternary Operator

--- Ternary_Operator.java
+++ Ternary_Operator.java
@@ -5,5 +5,14 @@ public class Ternary_Operator {
         // ternary operator ? = Return 1 of 2 values if a condition is true
 
         // variable = (condition) ? ifTrue : ifFalse;
-    }
+
+        int hours = 20;
+        String timeOfDay = (hours <12)? "A.M." : "P.M.";
+        System.out.println("Time of day: " + timeOfDay);
+
+        int income = 80000;
+        double taxRate = (income < 50000) ? 0.25 : 0.35;
+        System.out.println("Tax Rate: " + taxRate);
+
+        }
 }
